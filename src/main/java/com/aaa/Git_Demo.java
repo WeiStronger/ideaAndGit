720d6ee23ba4292d4b44fbd35925edcbd7dcddf0
ww <17326952752@163.com>
2019-03-17 17:10:10 +0800

    添加提交给GitHub

---
+++ src/main/java/com/aaa/Git_Demo.java
@@ -0,0 +1,13 @@
+package com.aaa;
+
+/**
+ * @author Wei
+ * @date 2019/3/17 17:03
+ */
+public class Git_Demo {
+    public static void main(String[] args) {
+
+        System.out.println("IDEA与Git整合");
+
+    }
+}
